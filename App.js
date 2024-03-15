f7d46c7f964935ee6ba1b6cc6fbc4cb8745a3626
Padma <padmarajugangurmallesha@gmail.com>
2024-03-15 16:19:17 +0000

    initial setup 1

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>New Progress is Good Progress</Text>
-      <StatusBar style="auto" />
+    <View>
+      <View></View>
+      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
