807d7a23580f5b8b5ab9b76354b3ae9c78d5b58f
Padma <padmarajugangurmallesha@gmail.com>
2024-03-18 12:11:17 +0000

    basics to flexbox

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View></View>
-      <View></View>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Course Goals" />
+        <Button title="Add Goals" />
+      </View>
+      <View>
+        <Text>Goals List</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
+  },
 });
